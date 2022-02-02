d4ec7f1a0ee8d0ade0fc873ef453f33c6fd13f18
Patryk <patryk.gielcz@gmail.com>
2022-02-02 14:34:39 +0100

    query listing guests

--- Hotel/Models/Guest.cs
+++ Hotel/Models/Guest.cs
@@ -2,7 +2,7 @@
 {
     public class Guest
     {
-        public int Id { get; set; }
+        public int ID { get; set; }
         public string Name { get; set; }
         public double Balance { get; set; }
     }
