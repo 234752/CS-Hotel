fa4fa309cfbf2a4f0f1d890a7050ab876a690405
Patryk <patryk.gielcz@gmail.com>
2022-02-07 19:58:22 +0100

    guest class

--- HotelWF/HotelForm.cs
+++ HotelWF/HotelForm.cs
@@ -1,8 +1,8 @@
 namespace HotelWF
 {
-    public partial class Form1 : Form
+    public partial class HotelForm : Form
     {
-        public Form1()
+        public HotelForm()
         {
             InitializeComponent();
         }
