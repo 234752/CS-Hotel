9eea1e2b98548a87826f0c30dfbe95ef88d2fb72
Patryk <patryk.gielcz@gmail.com>
2022-02-09 18:17:45 +0100

    showing all guests from single room

--- HotelWF/HotelForm.cs
+++ HotelWF/HotelForm.cs
@@ -11,10 +11,22 @@ namespace HotelWF
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Guest g1 = new Guest("bob", 200);
-            this.GuestGrid.Rows.Add();
-            this.GuestGrid.Rows[0].Cells[0].Value=g1.getName();
-            this.GuestGrid.Rows[0].Cells[1].Value=g1.getBalance();
+            Room r1 = new Room(1, 30.4, 3);
+            Guest g1 = new Guest("bob1", 200.1);
+            Guest g2 = new Guest("bob2", 200.2);
+            Guest g3 = new Guest("bob3", 200.3);
+            Guest g4 = new Guest("bob4", 200.4);
+            r1.addGuest(g1);
+            r1.addGuest(g2);
+            r1.addGuest(g3);
+            r1.addGuest(g4);
+            for(int i = 0; i < r1.getNumberOfGuests(); i++)
+            {
+                this.GuestGrid.Rows.Add();
+                this.GuestGrid.Rows[i].Cells[0].Value = r1.Guests[i].getName();
+                this.GuestGrid.Rows[i].Cells[1].Value = r1.Guests[i].getBalance();
+            }
+            
         }
     }
 }
