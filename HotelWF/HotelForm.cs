eef615651a9fd72bd9d49b7ddf0e9fcf9a792310
Patryk <patryk.gielcz@gmail.com>
2022-02-10 17:35:14 +0100

    event for display button

--- HotelWF/HotelForm.cs
+++ HotelWF/HotelForm.cs
@@ -12,16 +12,9 @@ namespace HotelWF
         Hotel MainHotel = new Hotel();
         private void button1_Click(object sender, EventArgs e)
         {
-            int index=this.RoomGrid.CurrentCell.RowIndex;
-            
-            Room r1 = MainHotel.RoomList[index];
-            this.GuestGrid.Rows.Clear();
-            for(int i = 0; i < r1.getNumberOfGuests(); i++)
-            {
-                this.GuestGrid.Rows.Add();
-                this.GuestGrid.Rows[i].Cells[0].Value = r1.Guests[i].getName();
-                this.GuestGrid.Rows[i].Cells[1].Value = r1.Guests[i].getBalance();
-            }
+            int index = this.RoomGrid.CurrentCell.RowIndex;
+
+            GuestFunctions.DisplayGuests(MainHotel, GuestGrid, index);
             
         }
 
