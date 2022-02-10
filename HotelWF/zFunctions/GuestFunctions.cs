577437322de42ff9f4c984821851b50f81015d81
Patryk <patryk.gielcz@gmail.com>
2022-02-10 17:32:31 +0100

    display guest function

--- HotelWF/zFunctions/GuestFunctions.cs
+++ HotelWF/zFunctions/GuestFunctions.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotelWF.zClasses;
 
 namespace HotelWF.zFunctions
 {
     internal static class GuestFunctions
     {
-        public static void DisplayGuests()
+        public static void DisplayGuests(Hotel H, DataGridView DGV, int index)
         {
+            
 
+            Room r = H.RoomList[index];
+            DGV.Rows.Clear();
+            for (int i = 0; i < r.getNumberOfGuests(); i++)
+            {
+                DGV.Rows.Add();
+                DGV.Rows[i].Cells[0].Value = r.Guests[i].getName();
+                DGV.Rows[i].Cells[1].Value = r.Guests[i].getBalance();
+            }
         }
     }
 }
