9a59355b46606e080b52693a2ad9b97bc0bd1225
Patryk <patryk.gielcz@gmail.com>
2022-02-10 17:29:34 +0100

    function class for guest

---
+++ HotelWF/zFunctions/GuestFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelWF.zFunctions
+{
+    internal static class GuestFunctions
+    {
+        public static void DisplayGuests()
+        {
+
+        }
+    }
+}
