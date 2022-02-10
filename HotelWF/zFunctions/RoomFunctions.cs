849f8a8c7b7189a84e0f60c3b28178e023ece3ad
Patryk <patryk.gielcz@gmail.com>
2022-02-10 15:32:15 +0100

    first function class

---
+++ HotelWF/zFunctions/RoomFunctions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelWF.zClasses;
+
+namespace HotelWF.zFunctions
+{
+    internal class RoomFunctions
+    {
+    }
+}
