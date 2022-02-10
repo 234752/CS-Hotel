670e240a30606f959f7f9e443c76066d9ccc764c
Patryk <patryk.gielcz@gmail.com>
2022-02-10 14:46:26 +0100

    Hotel class

---
+++ HotelWF/zClasses/Hotel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelWF.zClasses
+{
+    internal class Hotel
+    {
+        public List<Room> RoomList { get; set; }
+        public int getRoomCount() { return RoomList.Count; }
+        public void addRoom(Room R)
+        {
+            RoomList.Add(R);
+        }
+        public bool removeRoom(Room R)
+        {
+            if (RoomList.Remove(R)) return true;
+            return false;
+        }
+    }
+}
