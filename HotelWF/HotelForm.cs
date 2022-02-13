c53a80aeb21e49df32ad59c6214948b49d6acd3b
Patryk <patryk.gielcz@gmail.com>
2022-02-13 20:21:53 +0100

    display rooms and guests combined into REFRESH() function

--- HotelWF/HotelForm.cs
+++ HotelWF/HotelForm.cs
@@ -12,6 +12,12 @@ namespace HotelWF
         Hotel MainHotel = new Hotel();
         Room CurrentRoom = new Room(999, 99.99, 9);
 
+        public void REFRESH()
+        {
+            RoomFunctions.displayRooms(MainHotel, RoomGrid);
+            GuestFunctions.displayGuests(MainHotel, GuestGrid, CurrentRoom);
+        }
+
         private void addRoomButton_Click(object sender, EventArgs e)
         {
             int n0 = 0;
@@ -22,7 +28,8 @@ namespace HotelWF
             Int32.TryParse(this.addRoomMaxGuestsInput.Text, out g0);
 
             MainHotel.addRoom(new RegularRoom(n0, a0, g0));
-            RoomFunctions.displayRooms(MainHotel, RoomGrid);
+            //RoomFunctions.displayRooms(MainHotel, RoomGrid);
+            REFRESH();
         }
 
         private void addGuestButton_Click(object sender, EventArgs e)
@@ -36,21 +43,24 @@ namespace HotelWF
 
             CurrentRoom.addGuest(new Guest(n0, b0));
 
-            GuestFunctions.displayGuests(MainHotel, GuestGrid, CurrentRoom);
+            //GuestFunctions.displayGuests(MainHotel, GuestGrid, CurrentRoom);
+            REFRESH();
         }
 
         private void displayGuestsButton_Click(object sender, EventArgs e)
         {
             int indexR = this.RoomGrid.CurrentCell.RowIndex;
             CurrentRoom = MainHotel.RoomList[indexR];
-            GuestFunctions.displayGuests(MainHotel, GuestGrid, CurrentRoom);
+            //GuestFunctions.displayGuests(MainHotel, GuestGrid, CurrentRoom);
+            REFRESH();
         }
 
         private void removeGuestButton_Click(object sender, EventArgs e)
         {
             int indexG = this.GuestGrid.CurrentCell.RowIndex;
             CurrentRoom.Guests.RemoveAt(indexG);
-            GuestFunctions.displayGuests(MainHotel, GuestGrid, CurrentRoom);
+            //GuestFunctions.displayGuests(MainHotel, GuestGrid, CurrentRoom);
+            REFRESH();
         }
     }
 }
