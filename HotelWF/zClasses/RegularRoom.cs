df75f3a40052eda6e206b7b1c5765b0959d6d839
Patryk <patryk.gielcz@gmail.com>
2022-02-13 19:51:25 +0100

    new inherited class

---
+++ HotelWF/zClasses/RegularRoom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelWF.zClasses
+{
+    internal class RegularRoom : Room
+    {
+        public RegularRoom(int n0, double a0, int m0) : base(n0, a0, m0) { }
+    }
+}
