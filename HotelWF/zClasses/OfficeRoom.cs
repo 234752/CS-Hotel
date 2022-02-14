4a3a19aaafcc6aef16c65a18638886a0cc76bc06
Patryk <patryk.gielcz@gmail.com>
2022-02-14 19:20:01 +0100

    inherited class Office

---
+++ HotelWF/zClasses/OfficeRoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelWF.zClasses
+{
+    internal class OfficeRoom : Room
+    {
+        public OfficeRoom(int n0, double a0, int m0) : base(n0, a0, 1) { }
+        public override double getFee()
+        {
+            if (getNumberOfGuests() == 1) return 300;
+            else return 0;
+        }
+    }
+}
