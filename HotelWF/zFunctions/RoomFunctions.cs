7cb737586cbb01fe9ffe1973c1fbf1b5e5588bfe
Patryk <patryk.gielcz@gmail.com>
2022-02-16 17:25:01 +0100

    updated displayRooms

--- HotelWF/zFunctions/RoomFunctions.cs
+++ HotelWF/zFunctions/RoomFunctions.cs
@@ -15,11 +15,12 @@ namespace HotelWF.zFunctions
             for (int i = 0; i < H.getRoomCount(); i++)
             {
                 DGV.Rows.Add();
-                DGV.Rows[i].Cells[0].Value = H.RoomList[i].getNumber();
-                DGV.Rows[i].Cells[1].Value = H.RoomList[i].getArea();
-                DGV.Rows[i].Cells[2].Value = H.RoomList[i].getMaxGuests();
-                DGV.Rows[i].Cells[3].Value = H.RoomList[i].getNumberOfGuests();
-                DGV.Rows[i].Cells[4].Value = H.RoomList[i].getFee();
+                DGV.Rows[i].Cells[0].Value = H.RoomList[i].getType();
+                DGV.Rows[i].Cells[1].Value = H.RoomList[i].getNumber();
+                DGV.Rows[i].Cells[2].Value = H.RoomList[i].getArea();
+                DGV.Rows[i].Cells[3].Value = H.RoomList[i].getMaxGuests();
+                DGV.Rows[i].Cells[4].Value = H.RoomList[i].getNumberOfGuests();
+                DGV.Rows[i].Cells[5].Value = H.RoomList[i].getFee();
             }
         }
     }
