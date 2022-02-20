0e3ce5fabfebf987823e389c4923f72b54665acf
Patryk <patryk.gielcz@gmail.com>
2022-02-20 11:31:05 +0100

    access list + addAccess method

--- HotelWF/zClasses/Guest.cs
+++ HotelWF/zClasses/Guest.cs
@@ -12,6 +12,7 @@ namespace HotelWF.zClasses
         private double Balance { get; set; }
         public string getName() { return Name; }
         public double getBalance() { return Balance; }
+        public List<Access> accesses { get; set; } = new List<Access> { null, null, null};
         public bool addBalance(double cash)
         {
             if (cash < 0) return false;
@@ -22,6 +23,10 @@ namespace HotelWF.zClasses
         {
             Name = name0;
             Balance = balance0;
+        }
+        public void addAccess(Access A, int index0)
+        {
+            accesses[index0] = A;
         }
     }
 }
