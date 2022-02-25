17a7b5bd10853f65b946725a5c866f0e404f15bf
Patryk <patryk.gielcz@gmail.com>
2022-02-25 18:56:18 +0100

    layout and new elements of form 2

--- HotelWF/EditingForm.Designer.cs
+++ HotelWF/EditingForm.Designer.cs
@@ -28,19 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Location = new System.Drawing.Point(171, 44);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(75, 23);
+            this.confirmButton.TabIndex = 0;
+            this.confirmButton.Text = "Confirm";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 23);
+            this.textBox1.TabIndex = 1;
             // 
             // EditingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(301, 215);
+            this.ClientSize = new System.Drawing.Size(306, 114);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.confirmButton);
             this.Name = "EditingForm";
             this.Text = "Edit Value";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button confirmButton;
+        private TextBox textBox1;
     }
 }
