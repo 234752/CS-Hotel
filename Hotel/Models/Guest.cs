02a09e56ba998b2bda3c4c0db2b1656031213f17
Patryk <patryk.gielcz@gmail.com>
2022-04-29 19:10:57 +0200

    removing base of not finished project

--- Hotel/Models/Guest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hotel.Models
-{
-    public class Guest
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public double Balance { get; set; }
-    }
-}
